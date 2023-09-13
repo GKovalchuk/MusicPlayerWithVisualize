26b72d19936b816e4b7a01146d55794569f6a390
GKovalchuk <101798645+GKovalchuk@users.noreply.github.com>
2023-09-13 04:57:03 +0300

    исправлена работа кнопки случайного порядка. Добавлена иконка для кнопки зацикливания. Файл playlistLogic переработан

--- src/js/playlistLogic.js
+++ src/js/playlistLogic.js
@@ -7,95 +7,102 @@ const shuffleButton = document.getElementById("shuffle");
 const loopButton = document.getElementById("loop");
 const nextButton = document.getElementById("next");
 const prevButton = document.getElementById("prev");
-// Случайный порядок.
-let shuffle = false;
-// Случайный порядок.
-let loop = false;
+
+// Воспроизведения трека из плейлиста.
+export const playTrack = (index) => {
+	// Получаем плейлист.
+	let playlist = loadPlaylist();
+	// Проверяем, что индекс в допустимом диапазоне.
+	if (index >= 0 && index < playlist.length) {
+		// Ищем нужный трек.
+		const track = playlist.filter((track) => index === track.queue)[0];
+
+		// Проверяем, трек находится в БД, или на сервере.
+		if (!track.keyDB) {
+			// Создаем элементы для нового трека.
+			buildAudioSrc(track);
+			// Воспроизводим трек.
+			audio.play();
+		} else {
+			// Открываем базу данных.
+			const request = indexedDB.open("audioDB", 1);
+			// Когда БД открыта.
+			request.onsuccess = function (event) {
+				// Получаем объект базы данных.
+				const db = event.target.result;
+				// Создаем транзакцию.
+				const transaction = db.transaction(["tracks"], "readonly");
+				// Получаем таблицу tracks.
+				const objectStore = transaction.objectStore("tracks");
+				// Отправляем запрос в таблицу по id.
+				const request = objectStore.get(track.keyDB);
+				// При успешном запросе.
+				request.onsuccess = function (event) {
+					// Получаем результат запроса.
+					const trackData = event.target.result;
+					// Проверяем.
+					if (trackData) {
+						// Передаем данные в src.
+						track.src = trackData.data;
+						// Создаем элементы для нового трека.
+						buildAudioSrc(track);
+						// Воспроизводим трек.
+						audio.play();
+					} else {
+						// Выводим сообщение об ошибке, если объектпустой.
+						console.log(`Track not found: ${track.name}`);
+					}
+				};
+				// Обрабатываем событие ошибки при получении объекта.
+				request.onerror = function (event) {
+					// Выводим сообщение об ошибке.
+					console.log(`Error getting track: ${event.target.errorCode}`);
+				};
+			};
+			// Обрабатываем событие ошибки при открытии базы данных
+			request.onerror = function (event) {
+				// Выводим сообщение об ошибке
+				console.log("Error opening database: " + event.target.errorCode);
+			};
+		}
+	} else {
+		// Если индекс недопустим, то выводим сообщение об ошибке.
+		console.error("Invalid track index");
+	}
+};
+
+// Создаем элементы для нового трека.
+const buildAudioSrc = (track) => {
+	audio.removeAttribute("src");
+	if (!track) {
+		console.error("трек не найден");
+		return;
+	}
+	// Устанавливаем атрибуты src и type у элемента audio.
+	audio.setAttribute("src", track.src);
+	audio.setAttribute("type", track.type);
+	// Рендер имени текущего трека.
+	renderTrackName(track);
+};
+
+// Рендер имени текущего трека.
+const renderTrackName = (track) => {
+	// Выводим имя на экран.
+	nameContainer.textContent = track.name;
+};
 
 export const playlistLogic = () => {
+	// Зацикливание.
+	let loop = false;
+	// Случайный порядок.
+	let shuffle = false;
+	let shuffleArr;
 	// Текущий индекс трека в плейлисте.
 	let currentTrack = 0;
 
-	// Получаем плейлист.
-	const playlist = loadPlaylist();
-
-	// Создаем элементы для нового трека.
-	const buildAudioSrc = (track) => {
-		audio.removeAttribute("src");
-		if (!track) {
-			console.error("трек не найден");
-			return;
-		}
-		// Устанавливаем атрибуты src и type у элемента audio.
-		audio.setAttribute("src", track.src);
-		audio.setAttribute("type", track.type);
-		// Рендер имени текущего трека.
-		renderTrackName(track);
-	};
-
-	// Воспроизведения трека из плейлиста.
-	const playTrack = (index) => {
-		// Проверяем, что индекс в допустимом диапазоне.
-		if (index >= 0 && index < playlist.length) {
-			// Ищем нужный трек.
-			const track = playlist.filter((track) => index === track.queue)[0];
-
-			// Проверяем, трек находится в БД, или на сервере.
-			if (!track.keyDB) {
-				// Создаем элементы для нового трека.
-				buildAudioSrc(track);
-				// Воспроизводим трек.
-				audio.play();
-			} else {
-				// Открываем базу данных.
-				const request = indexedDB.open("audioDB", 1);
-				// Когда БД открыта.
-				request.onsuccess = function (event) {
-					// Получаем объект базы данных.
-					const db = event.target.result;
-					// Создаем транзакцию.
-					const transaction = db.transaction(["tracks"], "readonly");
-					// Получаем таблицу tracks.
-					const objectStore = transaction.objectStore("tracks");
-					// Отправляем запрос в таблицу по id.
-					const request = objectStore.get(track.keyDB);
-					// При успешном запросе.
-					request.onsuccess = function (event) {
-						// Получаем результат запроса.
-						const trackData = event.target.result;
-						// Проверяем.
-						if (trackData) {
-							// Передаем данные в src.
-							track.src = trackData.data;
-							// Создаем элементы для нового трека.
-							buildAudioSrc(track);
-							// Воспроизводим трек.
-							audio.play();
-						} else {
-							// Выводим сообщение об ошибке, если объектпустой.
-							console.log(`Track not found: ${track.name}`);
-						}
-					};
-					// Обрабатываем событие ошибки при получении объекта.
-					request.onerror = function (event) {
-						// Выводим сообщение об ошибке.
-						console.log(`Error getting track: ${event.target.errorCode}`);
-					};
-				};
-				// Обрабатываем событие ошибки при открытии базы данных
-				request.onerror = function (event) {
-					// Выводим сообщение об ошибке
-					console.log("Error opening database: " + event.target.errorCode);
-				};
-			}
-		} else {
-			// Если индекс недопустим, то выводим сообщение об ошибке.
-			console.error("Invalid track index");
-		}
-	};
-
 	// Переключение на следующий трек в плейлисте.
 	const nextTrack = () => {
+		let playlist = loadPlaylist();
 		// Проверяем, что плейлист не пустой.
 		if (playlist.length > 0) {
 			// Увеличиваем текущий индекс на единицу.
@@ -106,7 +113,7 @@ export const playlistLogic = () => {
 			}
 			// При включенном shuffle идем по его списку.
 			if (shuffle) {
-				playTrack(shuffle[currentTrack]);
+				playTrack(shuffleArr[currentTrack]);
 			} else {
 				// Воспроизводим трек по новому индексу.
 				playTrack(currentTrack);
@@ -119,6 +126,7 @@ export const playlistLogic = () => {
 
 	// Переключение на предыдущий трек в плейлисте.
 	const prevTrack = () => {
+		let playlist = loadPlaylist();
 		// Проверяем, что плейлист не пустой.
 		if (playlist.length > 0) {
 			// Уменьшаем текущий индекс на единицу.
@@ -132,7 +140,7 @@ export const playlistLogic = () => {
 			}
 			// При включенном shuffle идем по его списку.
 			if (shuffle) {
-				playTrack(shuffle[currentTrack]);
+				playTrack(shuffleArr[currentTrack]);
 			} else {
 				// Воспроизводим трек по новому индексу.
 				playTrack(currentTrack);
@@ -140,20 +148,20 @@ export const playlistLogic = () => {
 		} else {
 			// Если плейлист пустой, то выводим сообщение об ошибке.
 			console.error("Empty playlist");
-			playTrack;
 		}
 	};
 
 	const shuffleTracks = () => {
-		if (!shuffle) {
+		if (shuffle) {
+			let playlist = loadPlaylist();
 			// Выдаем класс кнопке shuffle.
 			shuffleButton.classList.add("player__track-controls--active");
 			// Создаем массив для случайного порядка.
-			shuffle = [];
+			shuffleArr = [];
 			// Заполняем массив числами.
-			for (let i = 0; i < playlist.length; i++) shuffle.push(i);
+			for (let i = 0; i < playlist.length; i++) shuffleArr.push(i);
 			// Подготавливаем переменные для перемешивания.
-			let currentIndex = shuffle.length;
+			let currentIndex = shuffleArr.length;
 			let shuffleIndex;
 
 			// Перемешиваем.
@@ -164,17 +172,20 @@ export const playlistLogic = () => {
 				shuffleIndex = Math.floor(Math.random() * playlist.length);
 
 				// Меняем местами элементы.
-				[shuffle[currentIndex], shuffle[shuffleIndex]] = [shuffle[shuffleIndex], shuffle[currentIndex]];
+				[shuffleArr[currentIndex], shuffleArr[shuffleIndex]] = [shuffleArr[shuffleIndex], shuffleArr[currentIndex]];
 			}
 		} else {
 			// Удаляем класс кнопке shuffle.
 			shuffleButton.classList.remove("player__track-controls--active");
 			// Удаляем массив.
-			shuffle = false;
+			shuffleArr = [];
 		}
 	};
 
+	// Рендерим кнопки с треками в плейлисте.
 	const renderPlaylist = () => {
+		let playlist = loadPlaylist();
+		// Очищаем контейнер плейлиста.
 		playlistContainer.innerHTML = "";
 		const buttonList = [];
 		playlist.forEach((track) => {
@@ -189,14 +200,10 @@ export const playlistLogic = () => {
 			});
 			buttonList.push(button);
 		});
+		// Сортируем кнопки по очередности.
 		buttonList.sort((a, b) => a.queue - b.queue);
+		//Заполняем контенер кнопками плейлиста.
 		buttonList.forEach((button) => playlistContainer.append(button));
-	};
-
-	// Рендер имени текущего трека.
-	const renderTrackName = (track) => {
-		// Выводим имя на экран.
-		nameContainer.textContent = track.name;
 	};
 
 	// Зацикливание текущего трека.
@@ -235,30 +242,37 @@ export const playlistLogic = () => {
 		};
 	}
 
+	// Добавление дебаунсов.
 	const nextTrackWithDebounce = debounce(nextTrack, 100);
 	const prevTrackWithDebounce = debounce(prevTrack, 100);
 	const loopWithDebounce = debounce(() => {
 		if (shuffle) {
-			playTrack(shuffle[currentTrack]);
+			playTrack(shuffleArr[currentTrack]);
 		} else {
 			playTrack(currentTrack);
 		}
 	}, 100);
 
+	// Обновляем кнопки зацикливания и случайного порядка.
+	loopTrack();
+	shuffleTracks();
+
+	// Назначаем прослушиватели кнопке зацикливания и случайного порядка.
+	shuffleButton.onclick = () => {
+		shuffle = !shuffle;
+		shuffleTracks();
+	};
+	loopButton.onclick = () => {
+		loop = !loop;
+		loopTrack();
+	};
+
 	// Добавляем обработчики событий на кнопки next и prev.
 	nextButton.addEventListener("click", nextTrackWithDebounce);
 	prevButton.addEventListener("click", prevTrackWithDebounce);
-	shuffleButton.addEventListener("click", shuffleTracks);
-	loopButton.addEventListener("click", () => {
-		loop = !loop;
-		loopTrack();
-	});
-
 	// Добавляем к событию play запуск визуализации.
 	audio.addEventListener("play", visualisationAudio, { once: true });
 	audio.addEventListener("ended", nextTrackWithDebounce);
-
-	loopTrack();
 	// Перерисовываем плейлист при перезагрузке плейлиста.
 	renderPlaylist();
 };
